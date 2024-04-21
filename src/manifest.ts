e500e35cadf99965e88a10f39946efbf05ba8d59
Dylan Dang <endercraft2319@gmail.com>
2024-04-21 02:41:54 -0500

    update manifest version

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,12 +1,12 @@
-export const id = 'sphere-generator';
+export const id = 'sphere_generator';
 
 const manifest = {
     title: 'Sphere Generator',
     author: 'Dylan Dang',
-    description: 'Generates a textured sphere block model',
+    description: 'Generates a textured sphere block model.',
     icon: 'sports_volleyball',
     variant: 'both',
-    version: '0.0.1',
+    version: '0.1.0',
 } satisfies PluginOptions;
 
 export const { title, author, description, icon, variant, version } = manifest;
