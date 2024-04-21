fc110e624b34ac6ca85a550b154b7a2de58f865e
Dylan Dang <endercraft2319@gmail.com>
2024-04-21 01:14:54 -0500

    change to type import

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import httpModule from 'http';
-import manifest, { title, icon, description, id } from './manifest'
-import dialog from './dialog'
+import type httpModule from 'http';
+import manifest, { title, icon, description, id } from './manifest';
+import dialog from './dialog';
 
 const menuAction = new Action(`${id}-action`, {
     name: title,
     description,
     icon,
     condition: {
-        modes: ['edit']
+        modes: ['edit'],
     },
     click: () => dialog.show(),
 });
@@ -16,23 +16,24 @@ BBPlugin.register(id, {
     ...manifest,
     onload() {
         if (DEBUG && !window.devServer) {
-            console.log("Development server injected");
+            console.log('Development server injected');
             const http: typeof httpModule = require('http');
-            window.devServer = http.createServer((_, res) => {
-                res.writeHead(200);
-                Plugins.devReload();
-                console.log('Plugin reloaded');
-                res.end('Plugin reloaded');
-            }).listen(8080);
+            window.devServer = http
+                .createServer((_, res) => {
+                    res.writeHead(200);
+                    Plugins.devReload();
+                    console.log('Plugin reloaded');
+                    res.end('Plugin reloaded');
+                })
+                .listen(8080);
         }
 
         MenuBar.addAction(menuAction, 'tools');
-
     },
     onunload() {
         menuAction.delete();
         dialog.delete();
     },
-    oninstall() { },
-    onuninstall() { },
+    oninstall() {},
+    onuninstall() {},
 });
