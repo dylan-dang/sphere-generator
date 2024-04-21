e5b3665f4a53a4944251804285cdaad117ce8e5f
Dylan Dang <endercraft2319@gmail.com>
2024-04-20 21:26:36 -0500

    rename utils to common

--- src/common.ts
+++ src/common.ts
@@ -1,3 +1,12 @@
+export const SIDES: CubeFaceDirection[] = [
+    'north',
+    'south',
+    'west',
+    'east',
+    'up',
+    'down',
+];
+
 export function error(message: string): Error {
     Blockbench.showQuickMessage(message, 2000);
     return new Error(message);
