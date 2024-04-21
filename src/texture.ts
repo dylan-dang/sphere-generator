14e62636fedc8d6f9cd1e87d44a28a2b927d59c9
Dylan Dang <endercraft2319@gmail.com>
2024-04-21 00:56:34 -0500

    add adjustments

--- src/texture.ts
+++ src/texture.ts
@@ -1,3 +1,4 @@
+import { error } from './common';
 import { Options } from './dialog';
 import fragmentShader from './shaders/fragment.glsl';
 import vertexShader from './shaders/vertex.glsl';
@@ -25,6 +26,7 @@ const missing =
 interface FaceInfo {
     face: CubeFaceDirection;
     angle: Vector2;
+    adjustment: number;
 }
 
 interface TextureInfo {
@@ -33,12 +35,36 @@ interface TextureInfo {
 }
 
 const faces: FaceInfo[] = [
-    { face: 'north', angle: new Vector2(0, 0) },
-    { face: 'south', angle: new Vector2(0, (1 * Math.PI) / 2) },
-    { face: 'west', angle: new Vector2(0, (2 * Math.PI) / 2) },
-    { face: 'east', angle: new Vector2(0, (3 * Math.PI) / 2) },
-    { face: 'up', angle: new Vector2(Math.PI / 2, Math.PI) },
-    { face: 'down', angle: new Vector2(-Math.PI / 2, Math.PI) },
+    {
+        face: 'north',
+        angle: new Vector2(Math.PI / 2, Math.PI),
+        adjustment: -Math.PI / 2,
+    },
+    {
+        face: 'south',
+        angle: new Vector2(-Math.PI / 2, Math.PI),
+        adjustment: -Math.PI / 2,
+    },
+    {
+        face: 'west',
+        angle: new Vector2(0, (2 * Math.PI) / 2),
+        adjustment: -Math.PI / 2,
+    },
+    {
+        face: 'east',
+        angle: new Vector2(0, 0),
+        adjustment: Math.PI / 2,
+    },
+    {
+        face: 'up',
+        angle: new Vector2(0, (1 * Math.PI) / 2),
+        adjustment: 0,
+    },
+    {
+        face: 'down',
+        angle: new Vector2(0, (3 * Math.PI) / 2),
+        adjustment: Math.PI,
+    },
 ];
 
 export async function generateTextures(opts: Options) {
@@ -66,11 +92,28 @@ export async function generateTextures(opts: Options) {
     });
     const plane = new Mesh(planeGeometry, planeMaterial);
     scene.add(plane);
-    return faces.map(({ angle, face }): TextureInfo => {
+    return faces.map(({ angle, face, adjustment }): TextureInfo => {
         planeMaterial.uniforms.angle.value = angle;
         renderer.render(scene, camera);
-        const dataURL = renderer.domElement.toDataURL();
-        const texture = new Texture({ name: face }).fromDataURL(dataURL);
+        const texture = new Texture({ name: face });
+        texture.edit(
+            (canvas) => {
+                if (!(canvas instanceof HTMLCanvasElement))
+                    throw error('canvas expected');
+                canvas.width = canvas.height = textureLength;
+                const ctx = canvas.getContext('2d');
+                if (!ctx) throw error("context2d couldn't be created");
+                ctx.save();
+                ctx.translate(textureLength / 2, textureLength / 2);
+                ctx.rotate(adjustment);
+                ctx.translate(-textureLength / 2, -textureLength / 2);
+                ctx.translate(textureLength, 0);
+                ctx.scale(-1, 1);
+                ctx.drawImage(renderer.domElement, 0, 0);
+                ctx.restore();
+            },
+            { no_undo: true }
+        );
         texture.add();
         return { face, texture };
     });
